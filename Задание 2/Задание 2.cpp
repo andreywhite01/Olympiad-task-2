19196afb36462fa1097ab146d9df7db4104d73e4
Andrey Kandybo <87312623+andreywhite01@users.noreply.github.com>
2022-04-17 10:36:51 +0700

    Добавьте файлы проекта.

---
+++ Задание 2/Задание 2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("input.txt");
+    string feeders_str, poisonTime_str, totalMinutesToTest_str;
+    getline(fin, feeders_str, ',');
+    getline(fin, poisonTime_str, ',');
+    getline(fin, totalMinutesToTest_str);
+    int feeders = atoi(feeders_str.c_str());
+    int poisonTime = atoi(poisonTime_str.c_str());
+    int totalMinutesToTest = atoi(totalMinutesToTest_str.c_str());
+
+    int mouses = 1;
+    int stages = int(totalMinutesToTest / poisonTime);
+
+    while (pow(stages + 1, mouses) < feeders) {
+        mouses++;
+    }
+    ofstream fout;
+    fout.open("output.txt");
+
+    fout << mouses;
+}
